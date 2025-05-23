e0320571cf1068f1ce6b6c790f884fabed90f733
TUAN <btuancreator@gmail.com>
2025-05-23 08:48:43 +0700

    cập nhật lần 1

---
+++ main.js
@@ -0,0 +1,7 @@
+const users = [
+  { id: 1, name: "an", age: 30 },
+  { id: 2, name: "bình", age: 40 },
+  { id: 3, name: "châu", age: 45 },
+];
+const names = users.map((a) => a.name);
+console.log(names);
